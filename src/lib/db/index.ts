9f86bb26079b1337832f084f2ca7f7876141ea0b
Modestas Stirbys <modestaso@gmail.com>
2026-04-08 22:50:20 +0300

    handle connection ssl cert

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -11,7 +11,17 @@ import type { DB } from './schema';
 
 import { env } from '$env/dynamic/private';
 
-export const pool = new pg.Pool({ connectionString: env.POSTGRES_PRISMA_URL });
+const isLocal = env.POSTGRES_PRISMA_URL?.includes('localhost');
+const sslConfig = isLocal
+  ? false
+  : env.SUPABASE_SSL_CERT
+    ? { ca: env.SUPABASE_SSL_CERT.replace(/\\n/g, '\n'), rejectUnauthorized: true }
+    : { rejectUnauthorized: false };
+
+export const pool = new pg.Pool({
+  connectionString: env.POSTGRES_PRISMA_URL,
+  ssl: sslConfig,
+});
 const dialect = new PostgresDialect({ pool });
 
 export const db = new Kysely<DB>({
