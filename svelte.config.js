949cf2479a44177dda24ab6ca32f20390056e5bf
Modestas Stirbys <modestaso@gmail.com>
2026-04-08 21:27:47 +0300

    change svelte adapter to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { preprocessMeltUI, sequence } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const origin = process.env.ORIGINS || process.env.ORIGIN;
